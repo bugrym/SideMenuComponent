0f05bd2ecca24e565516fca7fa90aa7fb88377af
vbugrym <vbugrym@lohika.com>
2024-03-06 18:08:04 +0200

    Initial Commit

---
+++ SideMenuComponent/SideMenuComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuComponentApp.swift
+//  SideMenuComponent
+//
+//  Created by vbugrym on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
